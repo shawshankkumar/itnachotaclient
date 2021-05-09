1616642f11e96096cf78caf8831bcb9768584658
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-05-09 18:41:42 +0530

    🥅 404 page

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import '../index.scss';
-import logo from './INTERSTELLAR.jpg';
+import logo from './img1.jpg';
 import Footer from './footer';
 
 const Fourofour = (): JSX.Element => {
   return (
-    <section>
-      <div className="bg-cover h-screen">
-        <img src={logo} alt="hell" />
+    <div>
+      <section className="bg-black">
+        <div className="">
+          <img src={logo} alt="Oops, unable to load this." className="object-fill w-full h-auto" />
+          <body className="">
+            <main className="">
+              <div className="absolute top-5 left-1 text-x1 text-sans font-bold ml-5 text-white items-center justify-content p-3	text-opacity-75">
+                Not all those who wander are lost...
+                <br />
+                but you most definitely are my friend.
+              </div>
+            </main>
+            <div className=" fixed bottom-40 right-1 text-2xl text-mono font-bold ml-5 text-black items-center justify-content p-3	hover:text-opacity-100 text-opacity-20 transform hover:-translate-y-1 hover:-translate-x-3	">
+              Don&apos;t worry it&apos;s just a 404 Error. You are merely lost on the web.
+              <br /> Unlike Cooper, who got lost in a black-hole!
+            </div>
+          </body>
+        </div>
+      </section>
+      <div className="">
+        <footer className=" content-center">
+          <Footer />
+        </footer>
       </div>
-      <div className="absolute top-5 left-1 text-x1 text-sans font-bold ml-5 text-white items-center justify-content p-3	text-opacity-75">
-        Not all those who wander are lost...
-        <br />
-        but you most definitely are my friend.
-      </div>
-      <div className="absolute bottom-3 w-full ">
-        <Footer />
-      </div>
-      <div className="absolute top-10 right-5 text-3xl text-mono font-bold ml-5 text-black items-center justify-content p-3	hover:text-opacity-100 text-opacity-20 transform hover:-translate-y-1 hover:-translate-x-3	">
-        Don&apos;t worry it&apos;s just a 404 Error. You are merely lost on the web.
-        <br /> Unlike Cooper, who got lost in a black-hole!
-      </div>
-    </section>
+    </div>
   );
 };
 
